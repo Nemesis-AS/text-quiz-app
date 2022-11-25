53821b50349b8070f16a969bb7e77f2b38df53c5
Nemesis <shuklaakshatascom@gmail.com>
2022-11-25 13:41:54 +0530

    Added Multiple Question Support

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,97 @@
+// @todo: Add Negative Marking
+
+class Quiz {
+    constructor(questions = [], metadata = {}, title = "Quiz") {
+        this.questions = questions;
+        this.metadata = metadata;
+        this.title = title;
+
+        this.markedAnswers = this.questions.map(_item => null);
+        this.cursorPos = -1;
+    }
+
+    static fromObj(obj) {
+        if (!obj.questions) return null;
+        let questions = obj.questions;
+        let metadata = obj.metadata || {};
+        let title = obj.title || "Quiz";
+
+        return new Quiz(questions, metadata, title);
+    }
+
+    getNext() {
+        if (this.cursorPos >= this.questions.length) return null;
+
+        this.cursorPos++;
+        return this.questions[this.cursorPos];
+    }
+
+    getPrev() {
+        if (this.cursorPos <= 0) return null;
+
+        this.cursorPos--;
+        return this.questions[this.cursorPos];
+    }
+
+    getCurrent() {
+        return this.questions[this.cursorPos];
+    }
+
+    isLast() {
+        return this.cursorPos === this.questions.length;
+    }
+
+    isFirst() {
+        return this.cursorPos === 0;
+    }
+
+    markAnswer(quesIdx, option) {
+        this.markedAnswers[quesIdx] = option;
+    }
+
+    checkAnswers() {
+        return this.questions.reduce((correct, ques, idx) => {
+            if (ques.correct_option == this.markedAnswers[idx]) return correct + 1;
+            return correct;
+        }, 0);
+    }
+
+    getMarks() {
+        let correctQues = this.checkAnswers();
+        console.log(correctQues);
+        let quesMarks = this.metadata.ques_marks || 1;
+        return correctQues * quesMarks;
+    }
+
+    getTotalMarks() {
+        let quesMarks = this.metadata.ques_marks || 1;
+        return this.questions.length * quesMarks;
+    }
+
+    getResult() {
+        return {
+            correct: this.checkAnswers(),
+            totalQues: this.questions.length,
+            marks: this.getMarks(),
+            totalMarks: this.getTotalMarks()
+        };
+    }
+}
+
 const fileReader = new FileReader();
+let quiz;
 
 let questionDiv;
 let optionsDiv;
 let submitBtn;
 let uploadBtn;
 let jsonInput;
+let nextBtn;
+let prevBtn;
 
 // ==========================STATE VARIABLES================================
 
-let selectedOption = -1;
-let correctOption = -1;
+let currentQues;
 
 // =========================================================================
 
@@ -19,27 +101,39 @@ function main() {
     questionDiv = document.getElementById("question");
     optionsDiv = document.getElementById("options");
     submitBtn = document.getElementById("submit-btn");
+    finalSubmitBtn = document.getElementById("submitQuiz");
     uploadBtn = document.getElementById("uploadJson");
     jsonInput = document.getElementById("jsonInput");
+    nextBtn = document.getElementById("nextBtn");
+    prevBtn = document.getElementById("prevBtn");
 
-    submitBtn.addEventListener("click", onQuestionSubmitted);
+    submitBtn.addEventListener("click", onQuizSubmitBtnClicked);
+    finalSubmitBtn.addEventListener("click", onQuizSubmitted);
     uploadBtn.addEventListener("click", onUploadBtnClicked);
     jsonInput.addEventListener("change", onFileSelected);
     fileReader.addEventListener("load", onFileLoaded);
 
-    renderQuestion({
-        question: "Question",
-        options: ["A", "B", "C", "D"],
-        correct_option: 1
+    nextBtn.addEventListener("click", onNextBtnClicked);
+    prevBtn.addEventListener("click", onPrevBtnClicked);
+
+    document.querySelectorAll(".modal-close").forEach(btn => {
+        btn.addEventListener("click", closeModal);
     });
+
+    renderQuestion();
 }
 
-function onQuestionSubmitted(event) {
-    event.preventDefault();
-    if (selectedOption === -1) return;
+function onQuizSubmitBtnClicked(_event) {
+    openModal("submitModal");
+}
 
-    revealAnswers();
-    selectedOption = -1;
+function onQuizSubmitted(_event) {
+    closeModal({ target: { dataset: { target: "submitModal" } } });
+    const modal = document.getElementById("resultModal");
+    let result = quiz.getResult();
+    modal.getElementsByClassName("correct-ans")[0].textContent = `${result.correct}/${result.totalQues}`;
+    modal.getElementsByClassName("marks")[0].textContent = `${result.marks}/${result.totalMarks}`;
+    openModal("resultModal");
 }
 
 function onUploadBtnClicked(_event) {
@@ -54,40 +148,72 @@ function onFileSelected(event) {
 
 function onFileLoaded(event) {
     const res = JSON.parse(event.target.result);
-    renderQuestion(res);
+    quiz = Quiz.fromObj(res);
+    currentQues = quiz.getNext();
+    renderQuestion();
 }
 
 function onOptionClicked(event) {
     let optId = event.target.dataset.option - 1;
-    selectedOption = optId;
+    quiz.markAnswer(quiz.cursorPos, optId);
     optionsDiv.childNodes.forEach(option => option.classList.remove("selected"));
 
     event.target.classList.add("selected");
 }
 
-function renderQuestion(object) {
-    questionDiv.textContent = object.question;
+function onNextBtnClicked(_event) {
+    let ques = quiz.getNext();
+    if (ques) {
+        currentQues = ques;
+        renderQuestion();
+    }
+}
+
+function onPrevBtnClicked(_event) {
+    let ques = quiz.getPrev();
+    if (ques) {
+        currentQues = ques;
+        renderQuestion();
+    }
+}
+
+function renderQuestion() {
+    if (!currentQues) return;
+
+    questionDiv.textContent = currentQues.question;
     optionsDiv.innerHTML = "";
-    object.options.forEach((option, idx) => {
+
+    let markedIdx = quiz.markedAnswers[quiz.cursorPos];
+    currentQues.options.forEach((option, idx) => {
         let btn = document.createElement("button");
         btn.classList.add("option-btn");
         btn.dataset.option = idx + 1;
         btn.innerText = option;
         btn.addEventListener("click", onOptionClicked);
+        if (idx == markedIdx) btn.classList.add("selected");
 
         optionsDiv.appendChild(btn);
     });
-    correctOption = object.correct_option;
 }
 
-function revealAnswers() {
-    optionsDiv.childNodes.forEach((option, idx) => {
-        if (idx === correctOption) {
-            option.classList.add("correct");
-            option.classList.remove("selected");
-        } else if (idx === selectedOption) {
-            option.classList.add("wrong");
-            option.classList.remove("selected");
-        }
-    });
+// function revealAnswers() {
+//     optionsDiv.childNodes.forEach((option, idx) => {
+//         if (idx === correctOption) {
+//             option.classList.add("correct");
+//             option.classList.remove("selected");
+//         } else if (idx === selectedOption) {
+//             option.classList.add("wrong");
+//             option.classList.remove("selected");
+//         }
+//     });
+// }
+
+function openModal(modalId) {
+    document.getElementById("modalBg").classList.remove("hidden");
+    document.getElementById(modalId).classList.remove("hidden");
+}
+
+function closeModal(event) {
+    document.getElementById(event.target.dataset.target).classList.add("hidden");
+    document.getElementById("modalBg").classList.add("hidden");
 }
